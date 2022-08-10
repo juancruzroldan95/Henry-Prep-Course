0c6a49366ac1dd27963981b0244eeb943f8be9e2
juancruzroldan95 <juancruz.roldan19@gmail.com>
2022-08-10 11:07:13 -0300

    JSII homework done

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -174,14 +174,10 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (numero > 0){
-    var es_positivo = "Es positivo";
-    return es_positivo;
-  }
-  else if (numero < 0){
-    var es_negativo = "Es negativo";
-    return es_negativo;
-  }
+  if (numero > 0)
+    return 'Es positivo';
+  else if (numero < 0)
+    return 'Es negativo';
   else
     return false;
 }
